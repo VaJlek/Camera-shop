44edd628705cc8630bf8d420bf75a9a00f1039df
Valentin <maksimov.valentin@gmail.com>
2023-03-28 11:19:29 +0300

    fix types

--- project/src/store/app-process/selectors.ts
+++ project/src/store/app-process/selectors.ts
@@ -1,11 +1,11 @@
 import { createSelector } from 'reselect';
 import { NameSpace } from '../../const';
-import { Camera, Reviews } from '../../types/types';
+import { Reviews } from '../../types/types';
 import { State } from '../../types/state';
 import { getReviews } from '../rewiews-data/selectors';
 
 export const getModalState = (state: State): string => state[NameSpace.App].modalState;
-export const getSelectedCamera = (state: State): Camera | undefined => state[NameSpace.App].selectedCameraId;
+export const getSelectedCamera = (state: State) => state[NameSpace.App].selectedCameraId;
 export const getReviewsAmount = (state: State): number => state[NameSpace.App].reviewsAmount;
 export const getSortType = (state: State): string | null => state[NameSpace.App].sortType;
 export const getSortOrder = (state: State): string | null => state[NameSpace.App].sortOrder;
--- project/src/store/cameras-data/selectors.ts
+++ project/src/store/cameras-data/selectors.ts
@@ -1,13 +1,13 @@
 import { NameSpace } from '../../const';
 import { State } from '../../types/state';
-import { Camera, Cameras } from '../../types/types';
+import { Cameras } from '../../types/types';
 
 export const getCameras = (state: State): Cameras | [] => state[NameSpace.Cameras].cameras;
 export const getCamerasFetchStatus = (state: State): string => state[NameSpace.Cameras].camerasFetchStatus;
 
 export const getCamerasTotalCount = (state: State): number => state[NameSpace.Cameras].camerasTotalCount;
 
-export const getCamera = (state: State): Camera | undefined => state[NameSpace.Cameras].camera;
+export const getCamera = (state: State) => state[NameSpace.Cameras].camera;
 export const getCameraFetchStatus = (state: State): string => state[NameSpace.Cameras].cameraFetchStatus;
 
 export const getSimilar = (state: State): Cameras | [] => state[NameSpace.Cameras].similar;
